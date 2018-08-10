9c4d9caf2d2ae8a109dde6a40e3cab4120c351e7
Dima9X <eremov95@gmail.com>
2018-08-10 17:21:07 +0300

    comm

--- src/test/java/com/example/project/MainTest.java
+++ src/test/java/com/example/project/MainTest.java
@@ -1,9 +1,7 @@
 package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 
 class MainTest {
 
